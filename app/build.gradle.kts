976c7e04a165cf948ba3bf226ce27cbce1982ed5
kim4152 <108558921+kim4152@users.noreply.github.com>
2024-07-08 12:53:00 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,15 +36,6 @@ android {
 }
 
 dependencies {
-
-    val activity_version = "1.9.0"
-
-    
-    // Kotlin
-    implementation("androidx.activity:activity-ktx:$activity_version")
-
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
